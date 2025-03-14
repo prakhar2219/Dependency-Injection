ae92ccda99a984b088c6b8f7248a4330aa3708b0
prakhar2219 <welcomeprakhar@gmail.com>
2025-03-14 20:44:49 +0530

    bind added

--- app/src/main/java/com/example/dependencyinjection/UserRepositoryServiceModule.kt
+++ app/src/main/java/com/example/dependencyinjection/UserRepositoryServiceModule.kt
@@ -1,10 +1,11 @@
 package com.example.dependencyinjection
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-class UserRepositoryServiceModule {
+abstract class UserRepositoryServiceModule {
 //    since dagger ko pta hai ki sql kaa object kaise bnana hai since hmlog ne wha pr @inject kaa use kiya hua hai
     //    ,isliye wo se bna lega just hume usko methid me pass krna hoga
 //    aur jaise hi hum function ko call kre ,hume usme sql repo kaa object pass krna hoga
@@ -26,4 +27,8 @@ class UserRepositoryServiceModule {
 //    to use binds annotation we make the methods abstract and the class as well.
 //    which simply means that we have bind our userRepository with the sql repository
 //    binds annotation kaa use tbhi krte hai jb dagger uska object khud bna skta ho
+
+    @Binds
+    abstract fun getSqlService(sqlRepository: SQLRepository): UserRepository
+
 }
